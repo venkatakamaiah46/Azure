cd837a3f45fdb88c21cac61d35c1979aca4465f9
Venkata Kamaiah Bommisetti <124617030+venkatakamaiah46@users.noreply.github.com>
2023-07-24 13:33:36 +0530

    Implementing SQL Merge Operation
    Implementing SQL Merge Operation in Azure Databricks for getting Updates/inserts into the Destination Table

--- SCD2.sql
+++ SCD2.sql
@@ -12,7 +12,16 @@
 
 -- COMMAND ----------
 
-select * from old_and_target_for_scd_2
+-- MAGIC %python
+-- MAGIC from pyspark.sql.functions import col,lit
+-- MAGIC df=spark.sql('''select * from default.old_and_target_for_scd2''')                                        #converting table to dataframe
+-- MAGIC #df=spark.read.table('default.old_and_target_for_scd2').withColumn('end_dt',col('end_dt').cast('date')) #converting table to dataframe
+-- MAGIC df.write.mode('overwrite').option("overwriteSchema", "true").saveAsTable('old_and_target_for_scd_2')
+
+-- COMMAND ----------
+
+-- alter table metastore.default.old_and_target_for_scd2
+-- alter column end_dt date
 
 -- COMMAND ----------
 
@@ -31,7 +40,7 @@ MERGE into old_and_target_for_scd_2 as t using
 union all
 select u.*,null as pk from upsert_for_scd_2 u join old_and_target_for_scd_2 s on u.State=s.State and s.Is_current='Yes') s  --These records will be inserted
 on t.State=s.pk and t.Is_current='Yes'
-when matched then update set t.End_dt='19-07-2023',t.Is_current='No'
+when matched then update set t.End_dt= current_date() ,t.Is_current='No'
 when not matched then insert *
 
 -- COMMAND ----------
